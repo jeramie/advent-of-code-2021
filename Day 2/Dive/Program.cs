22b3b4fd69d2ee2a7418019af3a55b47f9b16e92
jeramie <jeramie@cascadingfusion.com>
2021-12-02 13:23:29 -0500

    day 2 challenge 2

--- Day 2/Dive/Program.cs
+++ Day 2/Dive/Program.cs
@@ -1,19 +1,47 @@
-﻿int horizontal = 0, vertical = 0;
+﻿void ChallengeOne() {
+    int horizontal = 0, vertical = 0;
 
-File.ReadAllLines(@"input.txt").ToList().ForEach(input =>
-{
-    var parts = input.Split(' ');
-    var amount = int.Parse(parts[1]);
-
-    switch (parts[0])
+    File.ReadAllLines(@"input.txt").ToList().ForEach(input =>
     {
-        case "forward":
-            horizontal += amount;
-            break;
-        default:
-            vertical += parts[0] == "up" ? -amount : amount;
-            break;
-    }
-});
+        var parts = input.Split(' ');
+        var amount = int.Parse(parts[1]);
 
-Console.WriteLine($"Final Position: h:{horizontal} v:{vertical}, product: {horizontal * vertical}");
+        switch (parts[0])
+        {
+            case "forward":
+                horizontal += amount;
+                break;
+            default:
+                vertical += parts[0] == "up" ? -amount : amount;
+                break;
+        }
+    });
+
+    Console.WriteLine($"Final Position: h:{horizontal} v:{vertical}, product: {horizontal * vertical}");
+}
+
+void ChallengeTwo() {
+    int horizontal = 0, vertical = 0, aim = 0;
+
+    File.ReadAllLines(@"input.txt").ToList().ForEach(input =>
+    {
+        var parts = input.Split(' ');
+        var amount = int.Parse(parts[1]);
+
+        switch (parts[0])
+        {
+            case "forward":
+                horizontal += amount;
+                vertical += aim * amount;
+                break;
+            default:
+                aim += parts[0] == "up" ? -amount : amount;
+                break;
+        }
+    });
+
+    Console.WriteLine($"Final Position: h:{horizontal} v:{vertical}, product: {horizontal * vertical}");
+}
+
+ChallengeOne();
+ChallengeTwo();
