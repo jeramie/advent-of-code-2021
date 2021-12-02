b4f68d2b0fba378aa5bdaa57f3c493b7ebd317f8
Jeramie <jeramie@cascadingfusion.com>
2021-12-02 12:26:19 -0500

    Day 2: Dive - Challenge 1

---
+++ Day 2/Dive/Program.cs
@@ -0,0 +1,19 @@
+﻿int horizontal = 0, vertical = 0;
+
+File.ReadAllLines(@"input.txt").ToList().ForEach(input =>
+{
+    var parts = input.Split(' ');
+    var amount = int.Parse(parts[1]);
+
+    switch (parts[0])
+    {
+        case "forward":
+            horizontal += amount;
+            break;
+        default:
+            vertical += parts[0] == "up" ? -amount : amount;
+            break;
+    }
+});
+
+Console.WriteLine($"Final Position: h:{horizontal} v:{vertical}, product: {horizontal * vertical}");
