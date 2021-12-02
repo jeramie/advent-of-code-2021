f6bab030dcaee7f163f46167877730fd13372f92
Jeramie Hallyburton <jeramie.hallyburton@orderease.com>
2021-12-02 11:26:08 -0500

    Day 1 Challenge - Sonar Sweep

---
+++ Day 1/SonarSweep/Program.cs
@@ -0,0 +1,30 @@
+﻿var inputs = File.ReadAllLines(@"input.txt").Select(int.Parse).ToList();
+
+var increases = 0;
+var prev = inputs[0];
+foreach (var input in inputs)
+{
+    if (input > prev) increases++;
+
+    prev = input;
+}
+
+Console.WriteLine($"Increases linear {increases}");
+
+
+var increases2 = 0;
+var prev2 = int.MaxValue;
+for (var i = 0; i < inputs.Count; i++)
+{
+    if (i + 2 < inputs.Count)
+    {
+        var sum = new[] { inputs[i], inputs[i + 1], inputs[i + 2] }.Sum();
+
+        if (sum > prev2) increases2++;
+
+        prev2 = sum;
+    }
+}
+
+
+Console.WriteLine($"Increases for sliding window {increases2}");
