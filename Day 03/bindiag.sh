d566398386c16b4140b20ffbeca7ff4a823785ac
Jeramie Hallyburton <jeramie@users.noreply.github.com>
2021-12-04 14:12:39 -0500

    Update bindiag.sh

--- Day 03/bindiag.sh
+++ Day 03/bindiag.sh
@@ -1,31 +1,29 @@
  #!/bin/bash
 
 # challenge 1
-while read -r line; do ((total++)) && for((i=0; i<${#line}; i++)) do ((counts[$i]+=${line:$i:1}));  done  done < input.txt
+while read -r line; do ((total++)) && for((i=0; i<${#line}; i++)) do ((counts[$i]+=${line:$i:1}));  done done < input.txt
 for((i=0; i<${#counts[@]}; i++)) do gamma+="$(( counts[$i] > $(( total / 2 )) ? 1 : 0))"; done
 echo "Submarine power consumption: $((2#$gamma * 2#$(echo $gamma | tr 01 10)))"
 
 # challenge 2
-while read -r line; do oxygen+=("$line") && co2+=("$line"); done < input.txt
+while read -r line; do inputs+=("$line"); done < input.txt
 
-for ((i = 0; i < ${#counts[@]}; i++))
-do
-    if [ ${#oxygen[@]} -gt 1 ]
-    then
-        bit=0
-        for((o = 0; o < ${#oxygen[@]}; o++)) do (( bit += ${oxygen[$o]:$i:1} )); done
-        bit=$(( bit == 1 || bit >= (${#oxygen[@]} + 2 -1) / 2 ? 1 : 0 ))
-        oxygen=( $( for r in ${oxygen[@]} ; do echo $r ; done | egrep "^.{$i}$bit" ) )
-    fi
+find_rating() {
+    arr=("$@")
+    for ((i = 0; i < ${#counts[@]}; i++))
+    do
+        if [ ${#arr[@]} -gt 1 ]
+        then
+            bit=0
+            for l in ${arr[@]}; do (( ${l:$i:1} )) && (( bit += ${l:$i:1} )); done
+            bit=$((bit >= (${#arr[@]} + 2 -1) / 2 ? $1 : (1 - $1)))
+            arr=( $( for r in ${arr[@]} ; do echo $r ; done | egrep "^.{$i}$bit" ) )
+        fi
+    done
+    echo "${arr[0]}"
+}
 
-    if [ ${#co2[@]} -gt 1 ]
-    then
-        bit=0
-        for((o = 0; o < ${#co2[@]}; o++)) do (( bit += ${co2[$o]:$i:1} )); done
-        bit=$(( (bit == 0 || bit >= (${#co2[@]} + 2 - 1) / 2)  ? 0 : 1 ))
-        co2=( $( for r in ${co2[@]} ; do echo $r ; done | egrep "^.{$i}$bit" ) )
-    fi
-done
+oxygen=$(find_rating 1 "${inputs[@]}")
+co2=$(find_rating 0 "${inputs[@]}")
 
-echo "Life support rating: $(( 2#${oxygen[0]} * 2#${co2[@]} ))"
-
+echo "Life support rating: $(( 2#${oxygen} * 2#${co2} ))"
