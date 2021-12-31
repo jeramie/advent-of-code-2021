84bdc0b21b59023b73e8acfee99f2f92963d0bbe
jeramie <jeramie@cascadingfusion.com>
2021-12-31 12:00:50 -0500

    Add more comments

--- Day 6/LanternFish/Program.cs
+++ Day 6/LanternFish/Program.cs
@@ -1,9 +1,10 @@
-﻿const int MAX_LIFE = 9;
+﻿const int MAX_LIFE_SIZE = 9; //size of the collection containing all days of the lifecycle. new fish are added at top of this cycle.
+const int RESET_LIFE_IDX = 6; //how many days a re-spawned fish has left (7 days since it is 0 indexed)
 
 static long GetCountOfFish(int days)
 {
     var fish = File.ReadAllLines(@"input.txt").SelectMany(input => input.Split(',').Select(int.Parse)).GroupBy(f => f)
-        .Aggregate(new List<long>(new long[MAX_LIFE]), (list, kv) =>
+        .Aggregate(new List<long>(new long[MAX_LIFE_SIZE]), (list, kv) =>
             {
                 list[kv.Key] = kv.Count();
                 return list;
@@ -11,15 +12,15 @@ static long GetCountOfFish(int days)
 
     for (var i = 0; i < days; i++)
     {
-        var expired = fish[0];
+        var expired = fish.First();
 
-        //shift array to decrement each fishes life
+        //shift list to decrement each fishes life
         fish.RemoveAt(0);
 
-        //expired fish are reset to life of 7 (6) days
-        fish[6] += expired;
+        //reset the lifecycle of the expired fish
+        fish[RESET_LIFE_IDX] += expired;
 
-        //each expired fish creates a new fish with life of 9 (8) days
+        //each expired fish creates a new fish at the top of the life cycle
         fish.Add(expired);
     }
 
