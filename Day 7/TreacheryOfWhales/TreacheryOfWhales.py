0501f074341215a0100cba4e61ac5ff9e7c5ede1
jeramie <jeramie@cascadingfusion.com>
2021-12-31 14:03:07 -0500

    fix median

--- Day 7/TreacheryOfWhales/TreacheryOfWhales.py
+++ Day 7/TreacheryOfWhales/TreacheryOfWhales.py
@@ -1,5 +1,4 @@
 import numpy as np
 data = [int(x) for x in open("input.txt", "r").read().split(",")]
-median = np.mode(data)
+median = np.median(data)
 print(f"The shortest amount of fuel spend is {sum([abs(median - i) for i in data])}")
-
