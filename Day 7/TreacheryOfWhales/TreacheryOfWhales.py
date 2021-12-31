aa3a6064ef026e6455dec99b7ad8dc7d40d9e47e
jeramie <jeramie@cascadingfusion.com>
2021-12-31 14:21:45 -0500

    Added Part 2

--- Day 7/TreacheryOfWhales/TreacheryOfWhales.py
+++ Day 7/TreacheryOfWhales/TreacheryOfWhales.py
@@ -1,4 +1,14 @@
 import numpy as np
+
 data = [int(x) for x in open("input.txt", "r").read().split(",")]
+
+#Part 1
 median = np.median(data)
-print(f"The shortest amount of fuel spend is {sum([abs(median - i) for i in data])}")
+print(f"Part 1: The shortest amount of fuel spend is {sum([abs(median - i) for i in data])}")
+
+#Part 2
+def sum_1_to_n(n): return n * (n + 1) / 2
+
+mean = int(np.mean(data))
+print(f"Part 2: The shortest amount of fuel spend is {sum([sum_1_to_n(abs(mean - i)) for i in data])}")
+
