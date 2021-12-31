46f243cb09ce82a822ee0296245ca56fa1262350
jeramie <jeramie@cascadingfusion.com>
2021-12-30 22:07:14 -0500

    Day 6 - Lantern Fish

--- Day 5/HydrothermalVent/Program.cs
+++ Day 5/HydrothermalVent/Program.cs
@@ -1,34 +1,33 @@
-﻿int GetCountOfOverlaps(bool includeDiagonals) =>
-    File.ReadAllLines(@"input.txt").SelectMany(input =>
+﻿int GetCountOfOverlaps(bool includeDiagonals) => File.ReadAllLines(@"input.txt").SelectMany(input =>
+{
+    var range = input.Split(" -> ");
+
+    var (x1, y1) = range.First().Split(',').Select(int.Parse).ToArray() switch { var i => (i.First(), i.Last()) };
+    var (x2, y2) = range.Last().Split(',').Select(int.Parse).ToArray() switch { var i => (i.First(), i.Last()) };
+
+    var minX = Math.Min(x1, x2);
+    var maxX = Math.Max(x1, x2) + 1;
+
+    if (x1 != x2 && y1 != y2)
     {
-        var range = input.Split(" -> ");
+        if (!includeDiagonals) return new List<(int, int)>();
 
-        var (x1, y1) = range.First().Split(',').Select(int.Parse).ToArray() switch { var i => (i.First(), i.Last()) };
-        var (x2, y2) = range.Last().Split(',').Select(int.Parse).ToArray() switch { var i => (i.First(), i.Last()) };
+        //determine direction of x and y
+        var yPos = y1 < y2;
+        var xPos = x1 < x2;
 
-        var minX = Math.Min(x1, x2);
-        var maxX = Math.Max(x1, x2) + 1;
+        return Enumerable.Range(x1, maxX - minX).Select(_ => (xPos ? x1++ : x1--, yPos ? y1++ : y1--));
+    }
 
-        if (x1 != x2 && y1 != y2)
-        {
-            if (!includeDiagonals) return new List<(int, int)>();
+    var isHorizontal = x1 == x2;
 
-            //determine direction of x and y
-            var yPos = y1 < y2;
-            var xPos = x1 < x2;
+    var minY = Math.Min(y1, y2);
+    var maxY = Math.Max(y1, y2) + 1;
 
-            return Enumerable.Range(x1, maxX - minX).Select(_ => (xPos ? x1++ : x1--, yPos ? y1++ : y1--));
-        }
+    return Enumerable.Range(isHorizontal ? minY : minX, isHorizontal ? maxY - minY : maxX - minX)
+        .Select(i => isHorizontal ? (minX, i) : (i, minY));
 
-        var isHorizontal = x1 == x2;
-
-        var minY = Math.Min(y1, y2);
-        var maxY = Math.Max(y1, y2) + 1;
-
-        return Enumerable.Range(isHorizontal ? minY : minX, isHorizontal ? maxY - minY : maxX - minX)
-            .Select(i => isHorizontal ? (minX, i) : (i, minY));
-
-    }).GroupBy(range => range).Count(p => p.Count() >= 2);
+}).GroupBy(range => range).Count(p => p.Count() >= 2);
 
 Console.WriteLine($"Data points with 2 or more overlaps (excluding diagonals): {GetCountOfOverlaps(false)}");
 
---
+++ Day 6/LanternFish/Program.cs
@@ -0,0 +1,29 @@
+﻿static long GetCountOfFish(int days)
+{
+    var fish = File.ReadAllLines(@"input.txt").SelectMany(input => input.Split(',').Select(int.Parse)).GroupBy(f => f)
+        .Aggregate(new List<long>(new long[9]), (list, kv) =>
+            {
+                list[kv.Key] = kv.Count();
+                return list;
+            });
+
+    for (var i = 0; i < days; i++)
+    {
+        var expired = fish[0];
+
+        //shift array to decrement each fishes life
+        fish.RemoveAt(0);
+
+        //expired fish are reset to life of 6 days
+        fish[6] += expired;
+
+        //each expired fish creates a new fish with life of 8 days
+        fish.Add(expired);
+    }
+
+    return fish.Sum(c => c);
+}
+
+Console.WriteLine($"After 80 days there are now {GetCountOfFish(80)}");
+
+Console.WriteLine($"After 256 days there are now {GetCountOfFish(256)}");
